7de3f77878ad17898c3d9ce07152a51ce3da48cd
尤雨东 <root@ltpp.vip>
2025-02-06 11:50:40 +0800

    feat:v0.4.0

--- src/file/write/async.rs
+++ src/file/write/async.rs
@@ -25,3 +25,28 @@ pub async fn async_write_to_file(file_path: &str, content: &[u8]) -> Result<(), 
     file.write_all(content).await?;
     Ok(())
 }
+
+/// Append the provided content to a file at the specified `file_path` asynchronously.
+///
+/// - `file_path`: The path to the file where the content will be written.
+/// - `content`: A byte slice (`&[u8]`) containing the content to be written to the file.
+///
+/// - Returns: `Ok(())` if the content was successfully written to the file, or an `Err` with the error details.
+///
+/// # Errors
+/// - If the file cannot be created or opened for writing, an error will be returned. This can happen if:
+///     - There is a problem with the file path (e.g., invalid or inaccessible path).
+///     - There are I/O issues when writing to the file.
+pub async fn async_append_to_file(file_path: &str, content: &[u8]) -> Result<(), Error> {
+    if let Some(parent_dir) = std::path::Path::new(file_path).parent() {
+        create_dir_all(parent_dir).await?;
+    }
+    let mut file = OpenOptions::new()
+        .write(true)
+        .create(true)
+        .append(true)
+        .open(file_path)
+        .await?;
+    file.write_all(content).await?;
+    Ok(())
+}
--- src/file/write/sync.rs
+++ src/file/write/sync.rs
@@ -29,3 +29,32 @@ pub fn write_to_file(file_path: &str, content: &[u8]) -> Result<(), Error> {
         .open(file_path)
         .and_then(|mut file| file.write_all(content))
 }
+
+/// Append the provided content to a file at the specified `file_path`.
+/// If the file does not exist, it will be created. If the file exists, the content will be appended to it.
+///
+/// # Parameters
+/// - `file_path`: The path to the file where the content will be written.
+/// - `content`: A byte slice (`&[u8]`) containing the content to be written to the file.
+///
+/// # Returns
+/// - `Result<(), Error>`:
+///     - `Ok(())`: If the content was successfully written to the file.
+///     - `Err(Error)`: If there was an error during file creation or writing.
+///
+/// # Errors
+/// - If the file cannot be created or opened for writing, an error will be returned. This can happen if:
+///     - There is a problem with the file path (e.g., invalid or inaccessible path).
+///     - There are I/O issues when writing to the file.
+#[inline]
+pub fn append_to_file(file_path: &str, content: &[u8]) -> Result<(), Error> {
+    if let Some(parent_dir) = std::path::Path::new(file_path).parent() {
+        create_dir_all(parent_dir)?;
+    }
+    OpenOptions::new()
+        .write(true)
+        .create(true)
+        .append(true)
+        .open(file_path)
+        .and_then(|mut file| file.write_all(content))
+}
