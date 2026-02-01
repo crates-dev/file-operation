1185eaddf7924799384041b005ccb27df62c6362
尤雨东 <root@ltpp.vip>
2026-02-01 09:40:22 +0800

    feat: v0.8.18

--- src/file/struct.rs
+++ src/file/struct.rs
@@ -1,7 +1,7 @@
 /// A wrapper struct for file data in string format.
 ///
 /// Provides convenient access to file contents as a string.
-#[derive(Debug, Clone)]
+#[derive(Clone, Debug, Default, Eq, Hash, Ord, PartialEq, PartialOrd)]
 pub struct FileDataString(
     /// The actual string content of the file.
     pub(crate) String,
